ad1b5c5b969000b9a021c3360a37ce6de03680a2
Eze Larry Ifeanyi <ezelarry379@gmail.com>
2023-01-13 02:07:12 +0100

    did stuff

--- src/main/java/com/larrex/batch/BatchConfig.java
+++ src/main/java/com/larrex/batch/BatchConfig.java
@@ -1,7 +1,10 @@
 package com.larrex.batch;
 
+import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,9 +15,30 @@ public class BatchConfig {
     @Autowired
     private StepBuilderFactory factory;
 
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Bean
+    public Job job() {
+
+        return jobBuilderFactory
+                .get("step1")
+                .incrementer(new RunIdIncrementer())
+                .listener(listener())
+                .start(step())
+                .build();
+
+    }
 
     public Step step() {
-        return factory.get("step1").<String, Step>chunk(1).reader(reader()).writer(writer()).processor(processor()).listener(listener()).build();
+        return factory
+                .get("step1")
+                .<String, String>chunk(1)
+                .reader(reader())
+                .writer(writer())
+                .processor(processor())
+                .listener(listener())
+                .build();
     }
 
     @Bean
